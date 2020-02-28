a10c382cbbed501eda3a98541ec8f201a6d7000a
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-02-27 21:06:50 -0500

    Fixing errors

---
+++ build/precache-manifest.c9347c2e40f5a4b10c0f33d9a2796cfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3ad7461b1ba83b9429267e7a11b603",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "25c94f76373f0da213d5",
+    "url": "/memory-game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f51236e934d673d155d8",
+    "url": "/memory-game/static/css/main.e9b73330.chunk.css"
+  },
+  {
+    "revision": "25c94f76373f0da213d5",
+    "url": "/memory-game/static/js/2.b98adbf5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/memory-game/static/js/2.b98adbf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51236e934d673d155d8",
+    "url": "/memory-game/static/js/main.87ef71f1.chunk.js"
+  },
+  {
+    "revision": "97d264b2bfb8f9c60f6b",
+    "url": "/memory-game/static/js/runtime-main.0ebb6092.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.888e9f95dbbe378345c58d6da7c943d9.js"
+  "/memory-game/precache-manifest.c9347c2e40f5a4b10c0f33d9a2796cfc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memory-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
